9e176a9021526f53ced8b86aed47996628c002d5
Louis Tschall <tschalllouis@gmail.com>
2020-05-17 12:01:33 +0200

    add resnet class

--- convert2trt.py
+++ convert2trt.py
@@ -13,3 +13,22 @@ resnet50_model.eval()
 model_trt = torch2trt(resnet50_model, [x])
 
 torch.save(model_trt.state_dict(), 'best_trt.pth')
+
+
+class ResNet(nn.Module):
+
+    def __init__(
+        self, arch, pretrained=False, progress=False, num_cls=8, **kwargs
+    ):
+        super().__init__()
+
+        if(arch == 'resnet18'):
+            self.model = torchvision.models.resnet18(num_classes=num_cls)
+        elif(arch == 'resnet50'):
+            self.model = torchvision.models.resnet50(num_classes=num_cls)
+        else:
+            raise NotImplementedError("Resnet to be implemented:", arch)
+
+    def forward(self, input_dict):
+        cls = self.model(input_dict["image"])
+        return cls
