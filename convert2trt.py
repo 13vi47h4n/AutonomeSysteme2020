0d814f5d00f2a4a4a8b6cf49313e264ab168127f
Louis Tschall <tschalllouis@gmail.com>
2020-05-17 14:58:11 +0200

    fix dimension

--- convert2trt.py
+++ convert2trt.py
@@ -18,7 +18,7 @@ def convert():
     resnet50_model.eval()
 
     #create dummy data
-    data = np.zeros((224, 224, 3), np.uint8)
+    data = torch.zeros((1, 3, 224, 224).cuda()
 
     loader = transforms.Compose([transforms.ToTensor()])
     data = loader(data).float()
@@ -47,62 +47,6 @@ class ResNet(nn.Module):
     def forward(self, input_dict):
         cls = self.model(input_dict["image"])
         return cls
-
-class ResNetModel:
-
-    dictionary = {
-        "Contempt": 7,
-        "Anger": 6,
-        "Disgust": 5,
-        "Fear": 4,
-        "Surprise": 3,
-        "Sadness": 2,
-        "Happiness": 1,
-        "Neutral": 0,
-    }
-
-    def __init__(self, size=224, mode='gpu'):
-        resnet50_model = ResNet('resnet50')
-        PATH = './best.pth'
-        if torch.cuda.is_available() and mode == 'gpu':
-            checkpoint = torch.load(PATH)
-        else:
-            checkpoint = torch.load(PATH, map_location="cpu")
-
-        resnet50_model.load_state_dict(checkpoint['model_state_dict'])
-        resnet50_model.eval()
-        if mode == 'gpu':
-            self.device = torch.device(
-                "cuda:0" if torch.cuda.is_available() else "cpu")
-        else:
-            self.device = torch.device("cpu")
-        self.resnet50_model = resnet50_model.to(self.device)
-
-        self.label_map = dict((v, k) for k, v in self.dictionary.items())
-        self.size = size
-
-    def image_loader(self, image):
-        loader = transforms.Compose([transforms.ToTensor()])
-        image = loader(image).float()
-        image = image.unsqueeze(0)
-        return image
-
-    def resize_image(self, image):
-        image = cv2.resize(image, (self.size, self.size))
-        return image
-
-    def face_expression(self, image):
-        resized_image = self.resize_image(image)
-        tensor_image = self.image_loader(resized_image)
-        tensor_image = tensor_image.to(self.device)
-        input_image = {"image": tensor_image}
-        with torch.no_grad():
-            outputs = self.resnet50_model.forward(input_image)
-            _, predicted = torch.max(outputs, 1)
-            idx = predicted.item()
-            face_expression = self.label_map[idx]
-            return face_expression
-
 
 if __name__ == "__main__":
     convert()
