6563377e7e1360f49276ac174348851b89d2a532
Louis Tschall <tschalllouis@gmail.com>
2020-05-17 11:11:41 +0200

    reformat

--- convert2trt.py
+++ convert2trt.py
@@ -2,7 +2,6 @@ import torch
 from torch2trt import torch2trt
 from torchvision.models.alexnet import alexnet
 
-
 resnet50_model = ResNet('resnet50')
 PATH = './best.pth'
 checkpoint = torch.load(PATH)
